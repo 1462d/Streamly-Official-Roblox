5639d01295b7b21dcf8fcd489e7ca64a81c10729
Titan Studio <zseymour66@gmail.com>
2025-01-15 10:17:10 +1100

    Update Player.jsx

--- Player.jsx
+++ Player.jsx
@@ -1,51 +1,25 @@
-import React, { useEffect, useState } from 'react'
-import './Player.css'
-import back_arrow_icon from '../../assets/back_arrow_icon.png'
-import { useNavigate, useParams } from 'react-router-dom'
-import { TMDB_Access_Key } from '../../config'
- 
-const Player = (props) => {
-  const state = props.location;
-  const {id} = useParams();
-  const navigate = useNavigate();
+import React from 'react';
+import YouTube from 'react-youtube';
+import './Player.css';
 
-  const [apiData, setApiData] = useState({
-    name: "",
-    key: "",
-    published_at: "",
-    type: ""
-  })
-
-  const options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: `Bearer ${TMDB_Access_Key}`
-    }
+const Player = ({ videoId, onClose }) => {
+  const opts = {
+    height: '100%',
+    width: '100%',
+    playerVars: {
+      autoplay: 1,
+      controls: 1,
+    },
   };
 
-  useEffect(()=>{
-    fetch(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`, options)
-    .then(response => response.json())
-    .then(response => setApiData(response.results[0]))
-    .catch(err => console.error(err));
-    console.log(props);
-  },[])
-  
-  
-
   return (
-    <div className='player'>
-      <img src={back_arrow_icon} alt="" onClick={()=>{navigate(-2)}}/>
-      <iframe src={`https://www.youtube.com/embed/${apiData.key}`}
-      title='trailer' frameBorder='0' allowFullScreen></iframe>
-      <div className="player-info">
-        <p>{apiData.published_at.slice(0,10)}</p>
-        <p>{state}</p>
-        <p>{apiData.type}</p>
+    <div className="player-wrapper">
+      <div className="player-content">
+        <button className="close-btn" onClick={onClose}>×</button>
+        <YouTube videoId={videoId} opts={opts} />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Player
+export default Player;
